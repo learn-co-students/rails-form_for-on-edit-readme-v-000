2551184ed877fcf2a89e4f2874299b90a8531890
mmmeyers <marsi286@gmail.com>
2016-01-31 17:40:57 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit]
+  patch 'posts/:id', to: 'posts#update'
+  # you can either do patch, or add :update to the above array
 end
