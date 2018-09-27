ce12db31e9515ed76a9b504e64bbe8e127fa688e
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-09-27 19:31:51 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,11 +12,11 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new
-	  @post.title = params[:title]
-	  @post.description = params[:description]
-	  @post.save
-	  redirect_to post_path(@post)
+		@post = Post.new
+		@post.title = params[:title]
+		@post.description = params[:description]
+		@post.save
+		redirect_to post_path(@post)
 	end
 
 	def edit
@@ -24,8 +24,10 @@ class PostsController < ApplicationController
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
-	  redirect_to post_path(@post)
+		# binding.pry
+		@post = Post.find(params[:id])
+		# @post.update(title: params[:post][:title], description: params[:post][:description])
+		@post.update(params.require(:post))
+		redirect_to post_path(@post)
 	end
 end
