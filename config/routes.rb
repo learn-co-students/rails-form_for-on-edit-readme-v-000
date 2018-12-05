76c8e884e938f6331df1a36cc8292a579c177467
dani8439 <dani8439@aol.com>
2018-12-05 13:14:19 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit]
+  # or add update as additional argument to the resources method array, and the route below will happen automatically.
+
+  patch 'posts/:id', to: 'posts#update'
 end
