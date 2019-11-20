2d891847f1f46b25ea371bb4f10e3c91c0336034
sdcro <sdcrouse91@gmail.com>
2019-11-19 21:31:11 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,14 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
-	  redirect_to post_path(@post)
+	  # @post.update(title: params[:title], description: params[:description])
+    @post.update(params.require(:post).permit(:title, :description))
+
+    # Interesting. This works as well:
+    # @post.update(title: params[:post][:title], description: params[:post][:description])
+    # But THIS generates a ForbiddenAttributesError:
+    # @post.update(params[:post])
+
+    redirect_to post_path(@post)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  # put 'posts/:id', to: 'posts#update' 
+  # patch 'posts/:id', to: 'posts#update'
+  # Note: If I add an invalid route to the #only array, it won't show up in `rails routes`.
 end
